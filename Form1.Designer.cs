f1e860855899258bc42ee69d4a5de679b7a503f9
Boban Blagojević <blagojevic.boban@tsp.edu.rs>
2023-04-12 01:24:48 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace A3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSifra = new System.Windows.Forms.TextBox();
+            this.textBoxNaziv = new System.Windows.Forms.TextBox();
+            this.textBoxDatumPoc = new System.Windows.Forms.TextBox();
+            this.textBoxBudzet = new System.Windows.Forms.TextBox();
+            this.checkBoxZavrsen = new System.Windows.Forms.CheckBox();
+            this.textBoxOpis = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.buttonObrisi = new System.Windows.Forms.Button();
+            this.buttonIzadji = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1018, 47);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(171, 44);
+            this.toolStripButton1.Text = "Pregled projekata";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Šifra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Naziv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Datum početka";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Opis";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Završen";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Budžet";
+            // 
+            // textBoxSifra
+            // 
+            this.textBoxSifra.Location = new System.Drawing.Point(152, 66);
+            this.textBoxSifra.Name = "textBoxSifra";
+            this.textBoxSifra.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSifra.TabIndex = 7;
+            // 
+            // textBoxNaziv
+            // 
+            this.textBoxNaziv.Location = new System.Drawing.Point(152, 94);
+            this.textBoxNaziv.Name = "textBoxNaziv";
+            this.textBoxNaziv.Size = new System.Drawing.Size(205, 22);
+            this.textBoxNaziv.TabIndex = 8;
+            // 
+            // textBoxDatumPoc
+            // 
+            this.textBoxDatumPoc.Location = new System.Drawing.Point(152, 122);
+            this.textBoxDatumPoc.Name = "textBoxDatumPoc";
+            this.textBoxDatumPoc.Size = new System.Drawing.Size(141, 22);
+            this.textBoxDatumPoc.TabIndex = 9;
+            // 
+            // textBoxBudzet
+            // 
+            this.textBoxBudzet.Location = new System.Drawing.Point(152, 150);
+            this.textBoxBudzet.Name = "textBoxBudzet";
+            this.textBoxBudzet.Size = new System.Drawing.Size(205, 22);
+            this.textBoxBudzet.TabIndex = 10;
+            // 
+            // checkBoxZavrsen
+            // 
+            this.checkBoxZavrsen.AutoSize = true;
+            this.checkBoxZavrsen.Location = new System.Drawing.Point(152, 185);
+            this.checkBoxZavrsen.Name = "checkBoxZavrsen";
+            this.checkBoxZavrsen.Size = new System.Drawing.Size(18, 17);
+            this.checkBoxZavrsen.TabIndex = 11;
+            this.checkBoxZavrsen.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOpis
+            // 
+            this.textBoxOpis.Location = new System.Drawing.Point(152, 217);
+            this.textBoxOpis.Multiline = true;
+            this.textBoxOpis.Name = "textBoxOpis";
+            this.textBoxOpis.Size = new System.Drawing.Size(205, 137);
+            this.textBoxOpis.TabIndex = 12;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(390, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(595, 288);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // buttonObrisi
+            // 
+            this.buttonObrisi.Location = new System.Drawing.Point(748, 376);
+            this.buttonObrisi.Name = "buttonObrisi";
+            this.buttonObrisi.Size = new System.Drawing.Size(101, 51);
+            this.buttonObrisi.TabIndex = 14;
+            this.buttonObrisi.Text = "Obriši";
+            this.buttonObrisi.UseVisualStyleBackColor = true;
+            this.buttonObrisi.Click += new System.EventHandler(this.buttonObrisi_Click);
+            // 
+            // buttonIzadji
+            // 
+            this.buttonIzadji.Location = new System.Drawing.Point(884, 376);
+            this.buttonIzadji.Name = "buttonIzadji";
+            this.buttonIzadji.Size = new System.Drawing.Size(101, 51);
+            this.buttonIzadji.TabIndex = 15;
+            this.buttonIzadji.Text = "Izađi";
+            this.buttonIzadji.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sifra";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Naziv";
+            this.columnHeader2.Width = 188;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Datum pocetka";
+            this.columnHeader3.Width = 92;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Budzet";
+            this.columnHeader4.Width = 87;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Zavrsen";
+            this.columnHeader5.Width = 77;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Opis";
+            this.columnHeader6.Width = 117;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 452);
+            this.Controls.Add(this.buttonIzadji);
+            this.Controls.Add(this.buttonObrisi);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBoxOpis);
+            this.Controls.Add(this.checkBoxZavrsen);
+            this.Controls.Add(this.textBoxBudzet);
+            this.Controls.Add(this.textBoxDatumPoc);
+            this.Controls.Add(this.textBoxNaziv);
+            this.Controls.Add(this.textBoxSifra);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Projekat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSifra;
+        private System.Windows.Forms.TextBox textBoxNaziv;
+        private System.Windows.Forms.TextBox textBoxDatumPoc;
+        private System.Windows.Forms.TextBox textBoxBudzet;
+        private System.Windows.Forms.CheckBox checkBoxZavrsen;
+        private System.Windows.Forms.TextBox textBoxOpis;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button buttonObrisi;
+        private System.Windows.Forms.Button buttonIzadji;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            osveziListu();
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+                textBoxSifra.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                textBoxNaziv.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                textBoxDatumPoc.Text = listView1.SelectedItems[0].SubItems[2].Text;
+                textBoxBudzet.Text = listView1.SelectedItems[0].SubItems[3].Text;
+                if (listView1.SelectedItems[0].SubItems[4].Text == "True")
+                    checkBoxZavrsen.Checked = true;
+                else
+                    checkBoxZavrsen.Checked = false;
+                textBoxOpis.Text = listView1.SelectedItems[0].SubItems[5].Text;
+            }
+            else
+            {
+                textBoxSifra.Text = "";
+                textBoxNaziv.Text = "";
+                textBoxDatumPoc.Text = "";
+                textBoxBudzet.Text = "";
+                checkBoxZavrsen.Checked = false;
+                textBoxOpis.Text = "";
+            }
+        }
+
+        private void buttonObrisi_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection
+                (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\A3.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand
+                ("DELETE FROM Projekat WHERE ProjekatID=@sif", conn);
+            cmd.Parameters.AddWithValue("@sif",
+                Convert.ToInt32(textBoxSifra.Text));
+            // Provera parsovanjem, TryParse umesto Convert...
+            if(!checkBoxZavrsen.Checked)
+            {
+                MessageBox.Show("Projekat nije zavrsen!");
+                return;
+            }
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                //listView1.Items.Remove(listView1.SelectedItems[0]);
+                osveziListu();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greska: " + ex.Message);
+            }
+        }
+        private void osveziListu()
+        {
+            SqlConnection conn = new SqlConnection
+                (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\A3.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand
+                ("SELECT * FROM Projekat", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            try
+            {
+                da.Fill(dt);
+                listView1.Items.Clear();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ListViewItem item = new ListViewItem(dr[0].ToString());
+                    item.SubItems.Add(dr[1].ToString());
+                    item.SubItems.Add(((DateTime)dr[2]).ToString("dd.MM.yyyy"));
+                    item.SubItems.Add(dr[3].ToString());
+                    item.SubItems.Add(dr[4].ToString());
+                    item.SubItems.Add(dr[5].ToString());
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greska: " + ex.Message);
+            }
+        }
+    }
+}

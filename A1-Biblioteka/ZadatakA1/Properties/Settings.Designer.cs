8f55a0d194833176f3b8960938786b5d3c3a68ca
Boban Blagojević <blagojevic.boban@tsp.edu.rs>
2023-05-10 10:25:11 +0200

    ER dijagram - Project add new data source...

--- A1-Biblioteka/ZadatakA1/Properties/Settings.Designer.cs
+++ A1-Biblioteka/ZadatakA1/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ZadatakA1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,17 @@ namespace ZadatakA1.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\A1.mdf;Integr" +
+            "ated Security=True")]
+        public string A1ConnectionString {
+            get {
+                return ((string)(this["A1ConnectionString"]));
             }
         }
     }
